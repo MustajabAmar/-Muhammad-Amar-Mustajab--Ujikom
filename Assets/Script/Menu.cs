07f4e72da102b3f522adb4bd73f79c1b67bcee2e
Muhammad Amar Mustajab <m.amar.mustajab@gmail.com>
2023-12-14 12:59:14 +0700

    Source Code

---
+++ Assets/Script/Menu.cs
@@ -0,0 +1,13 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Play() => SceneManager.LoadScene("Game");
+
+
+}
